759206a7b69f9e1b5babe74a60eb87d6824dc330
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-10-13 20:08:26 +0600

    deployment changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import Image from 'next/image'
-import ChatComponent from '@/components/chatComponent'
+import ChatComponent from '@/components/chatComponent';
 
 export default function Home() {
-
-  // ChatComponent ? Why make a new component?
-  // ChatComponent -> client, text inputs -> onChange -> we need to make a client side component
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-slate-800 p-3 w-[800px] rounded-md text-white">
@@ -13,5 +8,5 @@ export default function Home() {
         <ChatComponent />
       </div>
     </main>
-  )
+  );
 }
